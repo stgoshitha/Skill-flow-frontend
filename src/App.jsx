e121d6140ce9083251a1153d087e65c02f2b5b0c
stgoshitha <wkgoshithapremachandra@gmail.com>
2025-05-10 19:20:20 +0530

    Add signup route to application

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Profile from './pages/Profile'
 import CreateHelpDeskForm from './pages/helpDeskPages/CreateHelpDeskForm'
 import MyHelpDesk from './pages/helpDeskPages/MyHelpDesk'
 import HelpDesk from './pages/helpDeskPages/HelpDesk'
+import Signup from './pages/Signup'
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
             <Route path="/profile" element={<Profile />} />
             <Route path="/helps" element={<CreateHelpDeskForm />} />
             <Route path="/myHelpDesk" element={<MyHelpDesk />} />
